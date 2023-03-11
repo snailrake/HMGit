ef4171e48d5deea4104bf6ad59f6cbe5e5bd7a88
snailrake <snailrake@mail.ru>
2023-03-11 12:56:45 +0400

    AddedClass

---
+++ TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+        //
+        // TODO: Add constructor logic here
+        //
+        public double sqrCircle()
+        {
+            return Math.PI * r * r;
+        }
+    }
+}
